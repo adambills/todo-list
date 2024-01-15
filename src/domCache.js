f91216af5cb1a903f6c4e3bf2e3b3b1265d56792
a-bills <121698448+adambills@users.noreply.github.com>
2024-01-15 15:37:28 -0500

    Add modal dialogs for new folders and tasks

---
+++ src/domCache.js
@@ -0,0 +1,8 @@
+const newFolder = document.querySelector("#new-folder");
+const newTask = document.querySelector("#new-task");
+const newFolderDialog = document.querySelector("#new-folder-dialog");
+const newTaskDialog = document.querySelector("#new-task-dialog");
+
+
+
+export  { newFolder, newTask, newFolderDialog, newTaskDialog };
--- src/index.js
+++ src/index.js
@@ -1,60 +1,11 @@
 import "./style.scss";
-// import Folder from "./Folder.js";
-// import { getSelectedFolder } from "./manageFolders.js";
+import { newFolder, newFolderDialog, newTask, newTaskDialog } from "./domCache";
 
+activateDialogButton(newFolder, newFolderDialog);
+activateDialogButton(newTask, newTaskDialog);
 
-
-// const folder2 = rootFolder.addNewFolder(
-//   "Folder 2",
-//   "Folder 2 Description",
-//   "red",
-//   activeFolder.level
-// );
-// const folder3 = folder2.addNewFolder(
-//   "Folder 3",
-//   "A Folder Inside Folder 2!",
-//   "purple",
-//   activeFolder.level
-// );
-// const item1 = rootFolder.addNewItem(
-//   "Test Item 1",
-//   "Description!",
-//   "Tomorrow",
-//   "High"
-// );
-// const item2 = folder3.addNewItem(
-//   "Test Item 2",
-//   "Description! Inside Folder 3!",
-//   "Next Year",
-//   "Low"
-// );
-// const folder4 = folder2.addNewFolder(
-//   "Folder 4",
-//   "Another Folder Inside Folder 2!",
-//   "green",
-//   activeFolder.level
-// );
-
-// const body = document.querySelector("body");
-
-// // need to add event listener recursively?
-// rootFolder.folderArr.forEach((folder) => {
-//   body.appendChild(folder.element);
-// });
-
-// folder2.folderArr.forEach((folder) => {
-//   rootFolder.appendChild(folder.element);
-// });
-
-
-
-// // set active folder
-// // activeFolder.addNewItem();
-// // activeFolder.addNewFolder();
-// // render UI element of folder structure
-// // ability to click on folder structure to move between folders
-// // back button to go up one level
-// // back button to go back to main
-// // activeFolder.removeItem(index);
-// // activeFolder.removeFolder(index);
-
+function activateDialogButton(button, dialog) {
+    button.addEventListener('click', ()=> {
+        dialog.showModal();
+    });
+}
